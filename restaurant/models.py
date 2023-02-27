f00e31b6a7b40bda70c9867243d153593c289ef7
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-27 15:29:45 -0400

    Add Booking and Menu models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime
 
-# Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(default=6)
+    BookingDate = models.DateField(default=timezone.now)
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(default=5)
+
+
