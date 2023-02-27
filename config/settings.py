08e2f38e23a869d63fe0bd5a69e68bdcf2ab9bd8
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-27 12:15:40 -0400

    Set up mysql database connection

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'admin',
+        'PASSWORD': '',
     }
 }
 
