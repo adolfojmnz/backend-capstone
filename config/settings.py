a954f241c624b8c80a16b99513591c3da1b2366b
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-27 15:29:18 -0400

    set options on database settings

--- config/settings.py
+++ config/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
         'PORT': '3306',
         'USER': 'admin',
         'PASSWORD': '',
+        'OPTIONS': {   
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
+        }, 
     }
 }
 
