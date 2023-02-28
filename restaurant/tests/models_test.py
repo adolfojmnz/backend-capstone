3df102519746d1d0b81020ff28179e70bb91c452
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-28 17:10:11 -0400

    Add test_created_items() on MenuTest class

--- restaurant/tests/models_test.py
+++ restaurant/tests/models_test.py
@@ -19,3 +19,9 @@ class MenuTest(TestCase):
             if self.items[idx].get('inventory'):
                 item.inventory = self.items[idx].get('inventory')
             item.save()
+
+    def test_created_items(self):
+        for idx in self.items.key():
+            inventory = 5 if self.items[idx].get('inventory') is None else self.items[idx]['inventory']
+            self.assertEqual(Menu.objects.get(id=idx).inventory, inventory)
+            self.assertEqual(Menu.objects.get(id=idx), f"{self.items[idx]['title']}: ${self.items[idx]['price']}")
