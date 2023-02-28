1eac4952005527339a2871ef1c5bfbe2791bc563
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-28 17:00:03 -0400

    Add __str__() on models

--- restaurant/models.py
+++ restaurant/models.py
@@ -7,8 +7,14 @@ class Booking(models.Model):
     no_of_guests = models.IntegerField(default=6)
     booking_date = models.DateField(default=timezone.now)
 
+    def __str__(self):
+        return f'{self.name}: {self.booking_date}'
+
 
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField(default=5)
+
+    def __str__(self):
+        return f'{self.title}: ${str(self.price)}'
