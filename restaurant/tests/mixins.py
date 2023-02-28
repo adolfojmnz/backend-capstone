bbff18d7f0aed9a846a71000e9f37279efb6d3a4
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-28 17:23:11 -0400

    Refactor: move code that create menu items into CreateMenuItemsMixin

---
+++ restaurant/tests/mixins.py
@@ -0,0 +1,19 @@
+from ..models import Menu
+
+class CreateMenuItemsMixin:
+    items = {
+        1: {'title': 'Icecream', 'price': 5.00, 'inventory': 7},
+        2: {'title': 'IrishCoffe', 'price': 7.89, 'inventory': 13},
+        3: {'title': 'Latte', 'price': 3.99, 'inventory': 0},
+        4: {'title': 'ApplePie', 'price': 13.78},
+    }
+
+    def create_menu_items(self):
+        for idx in self.items.keys():
+            item = Menu.objects.create(
+                title = self.items[idx]['title'],
+                price = self.items[idx]['price'],
+            )
+            if self.items[idx].get('inventory'):
+                item.inventory = self.items[idx].get('inventory')
+            item.save()
--- restaurant/tests/models_test.py
+++ restaurant/tests/models_test.py
@@ -1,27 +1,17 @@
 from django.test import TestCase
 
 from ..models import Menu
+from .mixins import CreateMenuItemsMixin
 
-class MenuTest(TestCase):
-    items = {
-        1: {'title': 'Icecream', 'price': 5.00, 'inventory': 7},
-        2: {'title': 'IrishCoffe', 'price': 7.89, 'inventory': 13},
-        3: {'title': 'Latte', 'price': 3.99, 'inventory': 0},
-        4: {'title': 'ApplePie', 'price': 13.78},
-    }
+
+class MenuTest(CreateMenuItemsMixin, TestCase):
 
     def setup(self):
-        for idx in self.items.keys():
-            item = Menu.objects.create(
-                title = self.items[idx]['title'],
-                price = self.items[idx]['price'],
-            )
-            if self.items[idx].get('inventory'):
-                item.inventory = self.items[idx].get('inventory')
-            item.save()
+        self.create_menu_items()
 
     def test_created_items(self):
         for idx in self.items.key():
             inventory = 5 if self.items[idx].get('inventory') is None else self.items[idx]['inventory']
             self.assertEqual(Menu.objects.get(id=idx).inventory, inventory)
             self.assertEqual(Menu.objects.get(id=idx), f"{self.items[idx]['title']}: ${self.items[idx]['price']}")
+    
