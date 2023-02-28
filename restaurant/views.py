e34f9b1c19ccac8629fef5518dcf1cb196319b07
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-27 21:56:14 -0400

    Create ViewSets for CRUD operations on Booking and Menu models

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Menu, Booking
+from .serializers import BookingSerializer, MenuSerializer
+
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemView(ListCreateAPIView):
+    model = Menu
+    queryset = model.objects.all()
+    serializer_class = MenuSerializer
+    # permission_classes = [IsAuthenticated]
+
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    model = Menu
+    queryset = model.objects.all()
+    serializer_class = MenuSerializer
+    # permission_classes = [IsAuthenticated]
+
+
+class BookingView(ListCreateAPIView):
+    model = Booking
+    queryset = model.objects.all()
+    serializer_class = BookingSerializer
+    # permission_classes = [IsAuthenticated]
+
+
+class SingleBookingView(RetrieveUpdateDestroyAPIView):
+    model = Booking
+    queryset = model.objects.all()
+    serializer_class = BookingSerializer
+    # permission_classes = [IsAuthenticated]
