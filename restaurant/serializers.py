5985951acebbdafbfa9c7d896e8b975449b6c593
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-27 21:55:22 -0400

    Create BookingSerializer and MenuSerializer

---
+++ restaurant/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Menu, Booking
+
+
+class BookingSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Booking
+        fields = ['name', 'no_of_guests', 'booking_date']
+
+
+class MenuSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Menu
+        fields = ['title', 'price', 'inventory']
