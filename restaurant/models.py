18877508ab29fbc7025b416a860e663d47ea959c
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-28 09:59:31 -0400

    Delete unrequired import

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils import timezone
-from datetime import datetime
 
 
 class Booking(models.Model):
@@ -13,5 +12,3 @@ class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField(default=5)
-
-
