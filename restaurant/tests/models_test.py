4ce44f60ea6e199870fff3ec5c64583a1d7b70a4
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-28 17:07:57 -0400

    Setup TestCate for Menu model

---
+++ restaurant/tests/models_test.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+
+from ..models import Menu
+
+class MenuTest(TestCase):
+    items = {
+        1: {'title': 'Icecream', 'price': 5.00, 'inventory': 7},
+        2: {'title': 'IrishCoffe', 'price': 7.89, 'inventory': 13},
+        3: {'title': 'Latte', 'price': 3.99, 'inventory': 0},
+        4: {'title': 'ApplePie', 'price': 13.78},
+    }
+
+    def setup(self):
+        for idx in self.items.keys():
+            item = Menu.objects.create(
+                title = self.items[idx]['title'],
+                price = self.items[idx]['price'],
+            )
+            if self.items[idx].get('inventory'):
+                item.inventory = self.items[idx].get('inventory')
+            item.save()
