88dd632a444b920229327eec911412798ccf0962
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-01 14:23:06 -0400

    Add 'api' app

--- config/settings.py
+++ config/settings.py
@@ -19,10 +19,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party apps
     'rest_framework',
     'rest_framework_simplejwt',
     'djoser',
+
+    # my apps
     'restaurant',
+    'api',
 ]
 
 MIDDLEWARE = [
