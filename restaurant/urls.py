0ef3eb3e68ca0e94b3e4823447d8f41c18471d4e
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-04 18:17:08 -0400

    Add about view

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.about, name="about"),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'restaurant/index.html', {})
+
+def about(request):
+    return render(request, 'restaurant/about.html')
