e38e7cd621d4e708f6a081a27e6b3d1cf55a508a
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-05 16:34:43 -0400

    Set DEFAULT_PERMISSION_CLASSES

--- config/settings.py
+++ config/settings.py
@@ -104,6 +104,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
