7ee7f1c95ba52d07d23fe7ca1ff4d4daa7054731
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-05 16:39:39 -0400

    Add menu and menu_item views

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name="about"),
+    path('menu/', views.menu, name='menu'),
+    path('menu-item/<int:pk>/', views.menu_item, name='menu-detail')
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.urls import reverse
+import requests, json, os
+
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+
+def get_auth_header():
+    url = 'http://127.0.0.1:8000/api/token/login/'
+    data = {
+        'username': env('USERNAME'),
+        'password': env('PASSWORD')
+    }
+    response = requests.post(url, data=data)
+    response_dict = json.loads(response.text)
+    token = response_dict.get('access')
+    return {'Authorization': f'JWT {token}'}
 
 
 def index(request):
     return render(request, 'restaurant/index.html', {})
 
+
 def about(request):
     return render(request, 'restaurant/about.html')
+
+
+def menu(request):
+    url = f"http://127.0.0.1:8000{reverse('api:menu')}"
+    headers = get_auth_header()
+    response = requests.get(url, headers=headers)
+    return render(request, 'restaurant/menu.html', context={'menu_items': json.loads(response.text)})
+
+def menu_item(request, pk):
+    url = f"http://127.0.0.1:8000{reverse('api:menu-detail', kwargs={'pk': pk})}"
+    headers = get_auth_header()
+    response = requests.get(url, headers=headers)
+    return render(request, 'restaurant/menu_item.html', context={'menu_item': json.loads(response.text)})
