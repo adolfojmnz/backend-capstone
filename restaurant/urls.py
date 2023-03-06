bc2241c66248f5ffb71c314ae970e8598a7bd41c
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-05 21:48:38 -0400

    Add Book class-based view

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('about/', views.about, name="about"),
     path('menu/', views.menu, name='menu'),
     path('menu-item/<int:pk>/', views.menu_item, name='menu-detail'),
+    path('book/', views.Book.as_view(), name='book'),
     path('bookings/', views.bookings, name='bookings'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.urls import reverse
 from django.utils import timezone
+from django.urls import reverse
+from django.views import View
+
+from .forms import BookingForm
+
 import requests, json
 
 import environ
@@ -41,6 +45,34 @@ def menu_item(request, pk):
     headers = get_auth_header()
     response = requests.get(url, headers=headers)
     return render(request, 'restaurant/menu_item.html', context={'menu_item': json.loads(response.text)})
+
+
+class Book(View):
+    form_class = BookingForm
+    template_name = 'restaurant/book.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def get_data_from_form(self, form):
+        data = {
+            'name': form.cleaned_data.get('name'),
+            'no_of_guests': form.cleaned_data.get('no_of_guests'),
+            'booking_date': form.cleaned_data.get('booking_date'),
+        }
+        return data
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            data = self.get_data_from_form(form)
+            headers = get_auth_header()
+            url = f"http://127.0.0.1:8000{reverse('api:bookings')}"
+            response = requests.post(url, data=data, headers=headers)
+            if response.status_code == 201:
+                return render(request, 'restaurant/book.html', context={'form': BookingForm()})
+        return render(request, 'restaurant/book.html', context={'form': form})
 
 
 def bookings(request):
